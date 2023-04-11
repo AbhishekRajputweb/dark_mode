6fc0ab3afde59ede3d15577e830026a0a749f24c
Abhishek Rajput <124500096+AbhishekRajputweb@users.noreply.github.com>
2023-04-11 22:13:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function white(){
+  document.getElementById("white").style.display = 'none';
+  document.getElementById("dark").style.display = 'block';
+}
+
+function dark(){
+    document.getElementById("white").style.display = 'block';
+    document.getElementById("dark").style.display = 'none';
+  }
